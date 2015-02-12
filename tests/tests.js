5754cf4b9aba5e6609032d076316093baf1e42ab
dequijada <daniventf@gmail.com>
2015-02-12 09:42:54 +0000

    Update tests.js

--- tests/tests.js
+++ tests/tests.js
@@ -6,10 +6,20 @@ suite('temperature', function() {
         calculate();
         assert.deepEqual(converted.innerHTML, "0.00 Celsius, 273.15 Kelvin");
     });
+    test('-15.4ºC = 4.28 Fahrenheit, 257.75 Kelvin', function() {
+        original.value = "-15.4ºC";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "4.28 Fahrenheit, 257.75 Kelvin");
+    });
     test('45C = 113.00 Fahrenheit, 318.15 Kelvin', function() {
         original.value = "45C";
         calculate();
         assert.deepEqual(converted.innerHTML, "113.00 Fahrenheit, 318.15 Kelvin");
+    });
+    test('0k = -273.15 Celsius, -459.67 Fahrenheit', function() {
+        original.value = "0k";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "-273.15 Celsius, -459.67 Fahrenheit");
     });
     test('5X = error', function() {
         original.value = "5X";
